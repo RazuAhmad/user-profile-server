7a4f2e427be37722360299f1ce09fd987fc1e8ff
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-04-27 20:05:32 +0600

    dot env file added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express =require('express');
+require("dotenv").config();
 const cors=require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
@@ -16,7 +17,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://userProfileDB:m1SMElK81JEEY5f5@cluster0.va5ejui.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.va5ejui.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
