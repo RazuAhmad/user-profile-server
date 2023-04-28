e4742a3abd21a1433c1c53831cf57f97fac7132d
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-04-28 14:31:23 +0600

    done almost

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const cors=require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
-const port =process.env.PORT || 5000;
+const port =process.env.PORT;
 
 // middleware
 app.use(cors());
